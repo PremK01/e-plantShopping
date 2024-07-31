38c29bf405998e4e80292eeac50200a9008bed5e
PremK01 <premprasadk01.com>
2024-07-31 04:50:16 -0400

    Configure Redux store with cartReducer

--- src/store.js
+++ src/store.js
@@ -1,9 +1,12 @@
-import { configureStore } from '@reduxjs/toolkit';
-import cartReducer from './CartSlice';
+// src/store.js
 
- const store = configureStore({
-    reducer: {
-        cart: cartReducer,
-    },
+import { configureStore } from '@reduxjs/toolkit'; // Import configureStore from @reduxjs/toolkit
+import cartReducer from './CartSlice'; // Import the cartReducer from CartSlice.jsx
+
+const store = configureStore({
+  reducer: {
+    cart: cartReducer, // Associate the cartReducer with the 'cart' key
+  },
 });
-export default store
+
+export default store; // Export the configured store
